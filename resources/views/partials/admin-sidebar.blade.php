dbf290bb9391fbccd17735f53ed3987da4714401
= <=>
2020-07-28 09:47:16 +0100

    changed icon of send mail

--- resources/views/partials/admin-sidebar.blade.php
+++ resources/views/partials/admin-sidebar.blade.php
@@ -19,7 +19,7 @@
             <li class=" nav-item"><a href="{{route('admin.users')}}"><i class="la la-users"></i><span class="menu-title"
                         data-i18n="nav.users.main">Users</span></a>
             </li>
-            <li class=" nav-item"><a href="{{route('admin.email')}}"><i class="la la-paper-plane"></i><span class="menu-title"
+            <li class=" nav-item"><a href="{{route('admin.email')}}"><i class="la la-envelope"></i><span class="menu-title"
                 data-i18n="nav.users.main">Mail</span></a>
             </li>
             <li class=" nav-item"><a href="{{route('admin.settings.index')}}"><i class="la la-cog"></i><span
