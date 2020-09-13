2cbb18f9d87e355732b781e6d5c51fe6c9b297b1
= <=>
2020-09-13 22:46:15 +0100

    modification to document upload algorithm

--- app/Http/Middleware/EnsureDocumentUpload.php
+++ app/Http/Middleware/EnsureDocumentUpload.php
@@ -15,24 +15,24 @@ class EnsureDocumentUpload
      */
     public function handle($request, Closure $next)
     {
-
+        session()->forget(['idTab','info']);
         if($user = auth()->user()){
             if($user->is_admin){
                 return $next($request);
-            }
-            if(count($user->documents) > 0){
-                if($user->accepted == 1 || $request->route()->named('users.document.upload')){
-                session()->forget(['info','idTab']);
+            }else if($user->accepted == 1){
                 return $next($request);
-                }
-                session()->forget(['info']);
+            }else if($request->route()->named('users.document.upload')){
+                return $next($request);
+            }else if(count($user->documents) > 0 && $user->accepted == 0){
                 session()->put('idTab',true);
                 session()->put('info','Your Document(s) are in review, we will notify you when we are done.');
                 return redirect()->route('profile');
+            }else{
+                session()->put('idTab',true);
+                session()->put('info','Upload your country\'s valid means of identication, or driver\'s license and/or international passport for verification');
+                return redirect()->route('profile');
             }
-            session()->put('idTab',true);
-            session()->put('info','Upload your country\'s valid means of identication, or driver\'s license and/or international passport for verification');
-            return redirect()->route('profile');
+
         }
         return redirect()->route('login');
     }
