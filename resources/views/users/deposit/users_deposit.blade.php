161f22876a7b3000e952566281d83b0c7b153fb2
lesley <leeayemi@gmail.com>
2021-03-09 01:09:09 +0100

    still on the testing process :)

--- resources/views/users/deposit/users_deposit.blade.php
+++ resources/views/users/deposit/users_deposit.blade.php
@@ -68,6 +68,7 @@
                 <div class="col-xs-12">
 
                     <!-- start -->
+
                     <div class="row">
                        <div class="col-xs-12">
                            <div class="text-center mt-30">
