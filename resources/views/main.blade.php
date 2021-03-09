3a8fc6dc991c20a2c70e923159bf404f947cec46
lesley <leeayemi@gmail.com>
2021-03-09 16:58:21 +0100

    removed video testimonials

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -525,7 +525,7 @@
        </div>
        <div class="row">
            <div class="blog-slider">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                {{-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="blog wow  fadeInUp animated" data-wow-duration="1.8s" style="visibility: visible; animation-duration: 1.8s; animation-name: fadeInUp;">
                         <figure>
                             <video class="owl-video video-js" preload="auto" style="width:100%; height: 370px;" src="/assets/front/images/testimonials/videos/new1.mp4" controls ></video>
@@ -535,7 +535,7 @@
                             <p>I invested with saxotradinngmarket and i got my profit.</p>
                         </div>
                     </div>
-                </div>
+                </div> --}}
                 {{-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="blog wow  fadeInUp animated" data-wow-duration="1.8s" style="visibility: visible; animation-duration: 1.8s; animation-name: fadeInUp;">
                         <figure>
@@ -566,18 +566,18 @@
                        </div>
                    </div>
                </div>
-               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+               {{-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="blog wow  fadeInUp animated" data-wow-duration="1.8s" style="visibility: visible; animation-duration: 1.8s; animation-name: fadeInUp;">
                         <figure>
                             <video class="owl-video video-js" preload="auto" style="width:100%; height: 370px;" src="/assets/front/images/testimonials/videos/new2.mp4" controls ></video>
-                            {{-- <a class="owl-video" href="/assets/front/images/testimonials/videos/1.mp4" ></a> --}}
+                            <a class="owl-video" href="/assets/front/images/testimonials/videos/1.mp4" ></a>
                         </figure>
                         <div class="content">
                             <h4><a href="#">Rebecca Jenkins</a></h4>
                             <p>I invested with saxotradinngmarket, and i got my profit.</p>
                         </div>
                     </div>
-               </div>
+               </div> --}}
 
                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                    <div class="blog wow  fadeInUp animated" data-wow-duration="1.8s" style="visibility: visible; animation-duration: 1.8s; animation-name: fadeInUp;">
@@ -615,7 +615,7 @@
                        </div>
                    </div>
                </div>
-               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+               {{-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="blog wow  fadeInUp animated" data-wow-duration="1.8s" style="visibility: visible; animation-duration: 1.8s; animation-name: fadeInUp;">
                     <figure>
                         <video class="owl-video video-js" preload="auto" style="width:100%; height: 370px;" src="/assets/front/images/testimonials/videos/3.mp4" controls ></video>
@@ -625,7 +625,7 @@
                         <p>I invested with saxotradinngmarket and i got my profit.</p>
                     </div>
                 </div>
-                </div>
+                </div> --}}
 
            </div>
        </div>
