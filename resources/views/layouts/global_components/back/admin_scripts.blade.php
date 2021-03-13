591278b547816eb241559ba312a4596dee5f9d66
Perekpo Emmanuel <perepaulonline@gmail.com>
2021-03-13 18:08:40 +0100

    tawk to account changed

--- resources/views/layouts/global_components/back/admin_scripts.blade.php
+++ resources/views/layouts/global_components/back/admin_scripts.blade.php
@@ -32,7 +32,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/603ec4da385de407571bdad1/default';
+s1.src='https://embed.tawk.to/604cda31067c2605c0b8013f/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
