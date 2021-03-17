be57642276e59070182c43375db0031bb1a0d033
Perekpo Emmanuel <perepaulonline@gmail.com>
2021-03-17 21:49:50 +0100

    chat changed to tidio

--- resources/views/layouts/global_components/front/front_scripts.blade.php
+++ resources/views/layouts/global_components/front/front_scripts.blade.php
@@ -29,37 +29,13 @@
 <script src="{{asset('assets/front/js/main.js')}}"></script>
 @auth
 @if(!auth()->user()->is_admin)
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/604cda31067c2605c0b8013f/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+<script src="//code.tidio.co/yy9rwgzzpoqdmrz7odi6vqrxtn71gsvs.js" async></script>
 @endif
 @endauth
 
 @guest
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/604cda31067c2605c0b8013f/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+<script src="//code.tidio.co/yy9rwgzzpoqdmrz7odi6vqrxtn71gsvs.js" async></script>
 
 @endguest
 
